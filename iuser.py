2e09081d219a6de33b23517ed7f4f473a6e0eeb3
ysajid <ysajidasfi@gmail.com>
2017-01-17 21:27:01 +0600

    start

---
+++ iuser.py
@@ -0,0 +1,16 @@
+import mechanize
+from bs4 import BeautifulSoup
+import urllib2 
+import cookielib
+
+cj = cookielib.CookieJar()
+br = mechanize.Browser()
+br.set_cookiejar(cj)
+br.open("http://10.220.20.12/index.php/home/login")
+
+br.select_form(nr=0)
+br.form['username'] = 'yeminsajid'
+br.form['password'] = 'abdullah'
+br.submit()
+
+print br.response().read()
