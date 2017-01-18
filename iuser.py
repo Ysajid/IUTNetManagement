bba731d7faebcb543c21f22ed9773bdf98d5e319
ysajid <ysajidasfi@gmail.com>
2017-01-18 12:05:09 +0600

    not the password :p

--- iuser.py
+++ iuser.py
@@ -10,7 +10,7 @@ br.open("http://10.220.20.12/index.php/home/login")
 
 br.select_form(nr=0)
 br.form['username'] = 'yeminsajid'
-br.form['password'] = 'abdullah'
+br.form['password'] = '-------'
 br.submit()
 
 print br.response().read()
