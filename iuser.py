5218870b0158996067144dc3e285b2ca51f8fc08
ysajid <ysajidasfi@gmail.com>
2017-01-26 22:45:28 +0600

    getting usage

--- iuser.py
+++ iuser.py
@@ -1,16 +1,28 @@
 import mechanize
+import re
 from bs4 import BeautifulSoup
 import urllib2 
 import cookielib
 
-cj = cookielib.CookieJar()
-br = mechanize.Browser()
-br.set_cookiejar(cj)
-br.open("http://10.220.20.12/index.php/home/login")
+def connect() : {
 
-br.select_form(nr=0)
-br.form['username'] = 'yeminsajid'
-br.form['password'] = '-------'
-br.submit()
+}
 
-print br.response().read()
+def getRemainingTime(userid, password) : 
+    cj = cookielib.CookieJar()
+    br = mechanize.Browser()
+    br.set_cookiejar(cj)
+    br.open("http://10.220.20.12/index.php/home/login")
+
+    br.select_form(nr=0)
+    br.form['username'] = userid
+    br.form['password'] = password
+    br.submit()
+
+    response  = br.response().read().replace('\n',' ')
+    usage = re.findall(r'<td>([0-9]+) Minute+', response)
+
+    return usage
+
+
+getRemainingTime("yeminsajid", "abdullah")
